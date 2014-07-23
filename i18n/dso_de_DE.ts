7b5526bc9de423f32987dc4e4b31cef19ffe9ac0
Hannes Matuschek <hmatuschek@gmail.com>
2014-07-23 17:16:13 +0200

    Added distribution stuff...

---
+++ i18n/dso_de_DE.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>DSOCtrlWidget</name>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="29"/>
+        <location filename="../src/dsoctrlwidget.cc" line="57"/>
+        <source>stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="32"/>
+        <location filename="../src/dsoctrlwidget.cc" line="60"/>
+        <source>start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="40"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="41"/>
+        <source>Trigger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="42"/>
+        <source>Plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="43"/>
+        <source>Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoctrlwidget.cc" line="44"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DSOInputCtrlWidget</name>
+    <message>
+        <location filename="../src/dsoinputctrlwidget.cc" line="30"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoinputctrlwidget.cc" line="31"/>
+        <source>Sample Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoinputctrlwidget.cc" line="46"/>
+        <location filename="../src/dsoinputctrlwidget.cc" line="53"/>
+        <source>%1 Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DSOPlotCtrlWidget</name>
+    <message>
+        <location filename="../src/dsoplotctrlwidget.cc" line="27"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoplotctrlwidget.cc" line="32"/>
+        <source>Time Frame (ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoplotctrlwidget.cc" line="33"/>
+        <source>History</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsoplotctrlwidget.cc" line="34"/>
+        <source>Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DSORecordCtrlWidget</name>
+    <message>
+        <location filename="../src/dsorecordctrlwidget.cc" line="20"/>
+        <source>&lt;none&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsorecordctrlwidget.cc" line="23"/>
+        <source>Enable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsorecordctrlwidget.cc" line="28"/>
+        <source>Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsorecordctrlwidget.cc" line="29"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DSOTriggerCtrlWidget</name>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="13"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="14"/>
+        <source>Rising Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="15"/>
+        <source>Falling Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="16"/>
+        <source>Exceeds Range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="48"/>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="104"/>
+        <source>-- Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="49"/>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="103"/>
+        <source>-- s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="54"/>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="151"/>
+        <source>%1 (%2%) s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="57"/>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="154"/>
+        <source>%1 (%2%) Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="60"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="67"/>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="70"/>
+        <source>Trigger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="68"/>
+        <source>Threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="69"/>
+        <source>Base</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="76"/>
+        <source>Count</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="77"/>
+        <source>Period</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="78"/>
+        <source>Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsotriggerctrlwidget.cc" line="81"/>
+        <source>Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
